3bf62a9ad0e745356ae19caa0a1d9853252f11f2
Alexk <morfem95@gmail.com>
2022-03-22 18:42:25 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const fibonacci = (n) =>{
+  if(n <= 0 || !n){
+     throw ('Wrong number');
+  }
     let a = 1, b = 1;
   for (let i = 3; i <= n; i++) {
     let c = a + b;
@@ -7,4 +10,4 @@ const fibonacci = (n) =>{
   }
   return b;
 }
-console.log(fibonacci(12));
+  console.log(fibonacci(0));
